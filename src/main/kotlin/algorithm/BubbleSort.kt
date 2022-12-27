b6270a8ad30a4c9633f4e4714a12ae35dbd905d5
ullee <ullee0917@gmail.com>
2022-12-28 02:12:01 +0900

    leetcode coding test
    - MinimumLines

--- src/main/kotlin/algorithm/BubbleSort.kt
+++ src/main/kotlin/algorithm/BubbleSort.kt
@@ -1,29 +1,28 @@
 package algorithm
 
 class BubbleSort {
-    private fun recursive(mNums: MutableList<Int>, lastIndex: Int) {
-        val swap = {arr: MutableList<Int>, source: Int, target: Int ->
-            val tmp = arr[source]
-            arr[source] = arr[target]
-            arr[target] = tmp
-        }
-        if (lastIndex > 0) {
+    private fun swap(arr: MutableList<Int>, source: Int, target: Int) {
+        val tmp = arr[source]
+        arr[source] = arr[target]
+        arr[target] = tmp
+    }
+
+    private tailrec fun recursive(mNums: MutableList<Int>, lastIndex: Int): MutableList<Int> {
+        return if (lastIndex > 0) {
             for (i in 1..lastIndex) {
                 if (mNums[i - 1] > mNums[i]) {
                     swap(mNums, i - 1, i)
                 }
             }
             recursive(mNums, lastIndex - 1)
-        }
+        } else mNums
     }
 
     fun bubbleSort(nums: List<Int>): List<Int> {
 
         val mNums = nums.toMutableList()
 
-        recursive(mNums, mNums.size - 1)
-
-        return mNums
+        return recursive(mNums, mNums.size - 1)
     }
 }
 
